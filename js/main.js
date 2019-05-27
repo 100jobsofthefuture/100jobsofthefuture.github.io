8dde1f2a7cdd58c20497cd69af5ed41a3de49893
iPadisthin <albert.penticoss@gmail.com>
2019-05-27 17:20:35 +1000

    updates

--- js/main.js
+++ js/main.js
@@ -1,4 +1,8 @@
 $(document).ready(function() {
+    $('.site-menu-toggle').on('click', function() {
+      $('.site-menu ul').slideToggle();
+    });
+
     $('.mobile-menu, .overlay').on('click', function() {
       $('.report aside').toggleClass('open');
       $('body').toggleClass('noScroll');
