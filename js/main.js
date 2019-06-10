5527bc360617f510b3b452ed83f3f4589c3e9c54
iPadisthin <albert.penticoss@gmail.com>
2019-06-10 14:55:04 +1000

    better modals

--- js/main.js
+++ js/main.js
@@ -3,8 +3,29 @@ $(document).ready(function() {
       $('.site-menu ul').slideToggle();
     });
 
-    $('.mobile-menu, .overlay').on('click', function() {
+    $('.mobile-menu').on('click', function() {
       $('.report aside').toggleClass('open');
       $('body').toggleClass('noScroll');
+    });
+
+    // job card details
+    $('.job-card').on('click', function(){
+      $(this).find('.job-card-full-descrip').toggle();
+      $('body').toggleClass('noScroll');
+    });
+
+    // job browse filters
+    $('.browse-filters .filter').on('click', function(){
+      var category = $(this).attr('id');
+      if(!$(this).hasClass('selected')){
+        $('.browse-filters .filter').removeClass('selected');
+        $(this).addClass('selected');
+        $('.job-card').hide();
+        if(category == 'all') {
+          $('.job-card').fadeIn();
+        } else {
+          $('.job-card.' + category + '').fadeIn();
+        }
+      }
     });
 });
